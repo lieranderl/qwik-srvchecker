11d42fb5f810f96159170486a42dda54c2addcd2
Evgenii Fedotov (evfedoto) <evfedoto@cisco.com>
2024-03-20 21:39:12 +0100

    done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ import { HiXCircleSolid } from "@qwikest/icons/heroicons";
 
 export const getUrl = () => {
   if (process.env.NODE_ENV === "production") {
-    return "https://europe-west1-srvchecker-collab.cloudfunctions.net/svrprocess-service";
+    return process.env.API_URL ?? ""
   }
   return "http://127.0.0.1:8080/srv_process";
 };
