809e30725a28feda416cf3a9879c36d6d2cf757f
Evgenii Fedotov (evfedoto) <evfedoto@cisco.com>
2024-03-21 15:11:45 +0100

    fmt

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ import { HiXCircleSolid } from "@qwikest/icons/heroicons";
 
 export const getUrl = () => {
   if (process.env.NODE_ENV === "production") {
-    return process.env.API_URL ?? ""
+    return process.env.API_URL ?? "";
   }
   return "http://127.0.0.1:8080/srv_process";
 };
